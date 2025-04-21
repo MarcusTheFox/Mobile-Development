52061cda3418ce56c7e7ba7cec67eec54a60ace5
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-04-22 01:23:49 +0300

    ServiceApp - completed

--- Lesson4/serviceapp/src/main/java/ru/mirea/bublikov/serviceapp/MainActivity.java
+++ Lesson4/serviceapp/src/main/java/ru/mirea/bublikov/serviceapp/MainActivity.java
@@ -1,24 +1,58 @@
 package ru.mirea.bublikov.serviceapp;
 
-import android.os.Bundle;
+import static android.Manifest.permission.FOREGROUND_SERVICE;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import ru.mirea.bublikov.serviceapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private int PermissionCode = 200;
+
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        boolean postNotificationsGranted = ContextCompat.checkSelfPermission(this,
+                POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED;
+        boolean foregroundServiceGranted = ContextCompat.checkSelfPermission(this,
+                FOREGROUND_SERVICE) == PackageManager.PERMISSION_GRANTED;
+
+        if (postNotificationsGranted && foregroundServiceGranted) {
+            Log.d(TAG, "Разрешения получены");
+        } else {
+            Log.d(TAG, "Нет разрешений!");
+            ActivityCompat.requestPermissions(this, new String[]{POST_NOTIFICATIONS, FOREGROUND_SERVICE}, PermissionCode);
+        }
+
+        binding.buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(MainActivity.this, PlayerService.class);
+                ContextCompat.startForegroundService(MainActivity.this, serviceIntent);
+            }
+        });
+
+        binding.buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(MainActivity.this, PlayerService.class);
+                stopService(serviceIntent);
+            }
         });
     }
 }
