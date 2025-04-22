7b9230aa1745d9447c2c93691ab1eacb8f5a873e
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-04-22 14:02:29 +0300

    WorkManager - completed

--- Lesson4/workmanager/src/main/java/ru/mirea/bublikov/workmanager/MainActivity.java
+++ Lesson4/workmanager/src/main/java/ru/mirea/bublikov/workmanager/MainActivity.java
@@ -1,24 +1,32 @@
 package ru.mirea.bublikov.workmanager;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.d(TAG, "MainActivity onCreate");
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresCharging(true)
+                .build();
+        WorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class)
+                .setConstraints(constraints)
+                .build();
+        WorkManager.getInstance(this)
+                .enqueue(uploadWorkRequest);
+        Log.d(TAG, "Задача SampleWorker поставлена в очередь WorkManager с ограничениями: " + constraints);
     }
 }
