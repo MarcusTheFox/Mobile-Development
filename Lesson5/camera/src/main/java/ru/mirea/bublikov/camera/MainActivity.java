bcfd6f29e17860ff6c833ac7eada34f4e61157a6
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-05-05 00:55:25 +0300

    Lesson5: Camera completed

--- Lesson5/camera/src/main/java/ru/mirea/bublikov/camera/MainActivity.java
+++ Lesson5/camera/src/main/java/ru/mirea/bublikov/camera/MainActivity.java
@@ -1,24 +1,130 @@
 package ru.mirea.bublikov.camera;
 
-import android.os.Bundle;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-import androidx.activity.EdgeToEdge;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import ru.mirea.bublikov.camera.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    private static final int CAMERA_REQUEST = 0;
+    private boolean isWork = false;
+    private Uri imageUri;
+    private ActivityMainBinding binding;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int cameraPermissionStatus = ContextCompat.checkSelfPermission(this, CAMERA);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
+
+        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Разрешение камеры получено.");
+            isWork = true;
+        } else {
+            Log.d(TAG, "Запрашиваем необходимые разрешения.");
+            ActivityCompat.requestPermissions(this, new String[]{
+                    CAMERA, WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_PERMISSION);
+        }
+
+        ActivityResultCallback<ActivityResult> callback = new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    binding.imageView.setImageURI(imageUri);
+                }
+            }
+        };
+        ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            callback);
+
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (isWork) {
+                    try {
+                        File photoFile = createImageFile();
+                        String authorities = getApplicationContext().getPackageName() + ".fileprovider";
+                        imageUri = FileProvider.getUriForFile(MainActivity.this, authorities, photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                        cameraActivityResultLauncher.launch(cameraIntent);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            Log.d(TAG, "onRequestPermissionsResult вызван. requestCode: " + requestCode);
+
+            boolean cameraPermissionGrantedResult = false;
+            for (int i = 0; i < permissions.length; i++) {
+                if (permissions[i].equals(CAMERA)) {
+                    if (grantResults.length > i && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        cameraPermissionGrantedResult = true;
+                        Log.d(TAG, "Результат запроса камеры: GRANTED");
+                    } else {
+                        Log.d(TAG, "Результат запроса камеры: DENIED");
+                    }
+                    break;
+                }
+            }
+
+            if (cameraPermissionGrantedResult) {
+                Log.d(TAG, "Разрешение камеры получено.");
+                isWork = true;
+            } else {
+                Log.w(TAG, "Разрешение камеры отклонено.");
+                isWork = false;
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDirectory);
     }
 }
