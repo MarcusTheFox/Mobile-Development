df4a1e8312aea28598d14fb7d099b3c934f547a2
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-05-06 13:35:00 +0300

    Lesson6: SecureSharedPreferences completed

--- Lesson6/securesharedpreferences/src/main/java/ru/mirea/bublikov/securesharedpreferences/MainActivity.java
+++ Lesson6/securesharedpreferences/src/main/java/ru/mirea/bublikov/securesharedpreferences/MainActivity.java
@@ -1,24 +1,51 @@
 package ru.mirea.bublikov.securesharedpreferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import ru.mirea.bublikov.securesharedpreferences.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private SharedPreferences secureSharedPreferences;
+    private static final String SECURE_PREFS_NAME = "secure";
+    private static final String KEY_POET_NAME = "poet_name";
+
+    private static final String DEFAULT_POET_NAME = "Гера Шипов";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        try {
+            KeyGenParameterSpec keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC;
+            String mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec);
+            secureSharedPreferences = EncryptedSharedPreferences.create(
+                    SECURE_PREFS_NAME,
+                    mainKeyAlias,
+                    getBaseContext(),
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+            secureSharedPreferences.edit().putString(KEY_POET_NAME, DEFAULT_POET_NAME).apply();
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
+
+        String poetName = secureSharedPreferences.getString(KEY_POET_NAME, "[Имя поэта]");
+        binding.textViewPoetName.setText(poetName);
+        binding.imageViewPoet.setImageResource(R.drawable.gera_chipov);
+
     }
 }
