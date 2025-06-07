53df7a653c1e96ca227896f5d3cbf50b70839942
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-06-07 20:24:16 +0300

    Lesson7: FirebaseAuth completed

--- Lesson7/firebaseauth/src/main/java/ru/mirea/bublikov/firebaseauth/MainActivity.java
+++ Lesson7/firebaseauth/src/main/java/ru/mirea/bublikov/firebaseauth/MainActivity.java
@@ -1,24 +1,182 @@
 package ru.mirea.bublikov.firebaseauth;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import ru.mirea.bublikov.firebaseauth.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.buttonCreateAccount.setOnClickListener(v -> {
+            String email = binding.editTextEmail.getText().toString();
+            String password = binding.editTextPassword.getText().toString();
+            createAccount(email, password);
         });
+
+        binding.buttonSignIn.setOnClickListener(v -> {
+            String email = binding.editTextEmail.getText().toString();
+            String password = binding.editTextPassword.getText().toString();
+            signIn(email, password);
+        });
+
+        binding.buttonSignOut.setOnClickListener(v -> signOut());
+
+        binding.buttonVerifyEmail.setOnClickListener(v -> sendEmailVerification());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            binding.statusTextView.setText(getString(R.string.emailpassword_status_fmt, user.getEmail(), user.isEmailVerified()));
+            binding.detailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            binding.detailTextView.setVisibility(View.VISIBLE);
+            binding.emailPasswordButtons.setVisibility(View.GONE);
+            binding.emailPasswordFields.setVisibility(View.GONE);
+            binding.signedInButtons.setVisibility(View.VISIBLE);
+            binding.buttonVerifyEmail.setEnabled(!user.isEmailVerified());
+        } else {
+            binding.statusTextView.setText(R.string.signed_out);
+            binding.detailTextView.setText(null);
+            binding.emailPasswordButtons.setVisibility(View.VISIBLE);
+            binding.emailPasswordFields.setVisibility(View.VISIBLE);
+            binding.signedInButtons.setVisibility(View.GONE);
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = binding.editTextEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            binding.editTextEmail.setError("Required.");
+            valid = false;
+        } else {
+            binding.editTextEmail.setError(null);
+        }
+
+        String password = binding.editTextPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            binding.editTextPassword.setError("Required.");
+            valid = false;
+        } else {
+            binding.editTextPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(MainActivity.this, "Аккаунт успешно создан.", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Ошибка создания аккаунта.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(MainActivity.this, "Авторизация успешна.", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Ошибка авторизации.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private void sendEmailVerification() {
+        binding.buttonVerifyEmail.setEnabled(false);
+
+        final FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            Toast.makeText(MainActivity.this, "Необходимо авторизоваться для верификации.", Toast.LENGTH_SHORT).show();
+            binding.buttonVerifyEmail.setEnabled(true);
+            return;
+        }
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        binding.buttonVerifyEmail.setEnabled(true);
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this,
+                                    "Письмо верификации отправлено на " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(MainActivity.this,
+                                    "Ошибка при отправке письма верификации.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
