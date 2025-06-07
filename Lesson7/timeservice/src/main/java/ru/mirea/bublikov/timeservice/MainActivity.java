c7ed04e883494d692cfbf41bda6bc08f91e88404
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-06-07 14:35:34 +0300

    Lesson7: TimeService completed

--- Lesson7/timeservice/src/main/java/ru/mirea/bublikov/timeservice/MainActivity.java
+++ Lesson7/timeservice/src/main/java/ru/mirea/bublikov/timeservice/MainActivity.java
@@ -1,24 +1,66 @@
 package ru.mirea.bublikov.timeservice;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+import ru.mirea.bublikov.timeservice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private final String host = "time.nist.gov";
+    private final int port = 13;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonGetTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                GetTimeTask timeTask = new GetTimeTask();
+                timeTask.execute();
+            }
         });
+    }
+
+    private class GetTimeTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String timeResult = "";
+            try {
+                Socket socket = new Socket(host, port);
+                BufferedReader reader = SocketUtils.getReader(socket);
+                reader.readLine();
+                timeResult = reader.readLine();
+                Log.d(TAG, timeResult);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return timeResult;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            String[] parts = result.split(" ");
+            if (parts.length < 3) return;
+
+            String date = parts[1];
+            String time = parts[2];
+            binding.textViewTime.setText("Дата: " + date + "\nВремя: " + time);
+        }
     }
 }
